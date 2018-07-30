275d6bc3f3f70820dea903138069c13795296472
LOTONGA Eliam <eliam.lotonga@ives.fr>
2018-07-30 15:20:31 +0200

    bug

--- app/Http/Controllers/UrlsController.php
+++ app/Http/Controllers/UrlsController.php
@@ -67,7 +67,7 @@ class UrlsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($shortUrl)
     {
         //request sql search shortUrl to the DB
         // throw exception that is manage in file App\Exceptions\Handler.php
